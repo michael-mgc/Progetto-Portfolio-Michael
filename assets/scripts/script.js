ad50c3a03b6db8cd42297dfc9abe5360543764b4
michael-mgc <michael.smj97@gmail.com>
2026-08-13 23:57:22 +0200

    Refactor responsive header and menu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,41 +4,19 @@ const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
 
 
-let showMenu = false;
-
-menuBtn.addEventListener('click', toggleMenu);
-
-
-function toggleMenu() { 
-    if(!showMenu){
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        navItems.forEach(item => item.classList.add('open'))
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    menuNav.classList.toggle('open');
+    navItems.forEach(item => item.classList.toggle('open'))
+});
 
 
-        showMenu = true;
-    } else {
-      
-        closeMenu();
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
 
-    }
- }
+        navItems.forEach(item=> item.classList.remove('open'));
+    });
 
-
- function closeMenu() { 
-    nav.classList.remove('open');
-    menuNav.classList.remove('open');
-    navItems.forEach(item => item.classList.remove('open'))
-
-    showMenu = false;
-  }
-
-  navItems.forEach(item => {
-    item.addEventListener('click', closeMenu);
-  });
-
-  window.addEventListener('resize', () => {
-    if (window.innerWidth >= 1024 && showMenu) {
-        closeMenu();
-    }
 });
