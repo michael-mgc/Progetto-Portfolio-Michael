3d19b1af6220a816963d0fb678c1af0cb96656c2
michael-mgc <michael.smj97@gmail.com>
2026-08-28 12:20:52 +0200

    fix: complete mandatory requirments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,36 +1,8 @@
-const menuBtn = document.querySelector('.menu-btn');
-const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelectorAll('.menu-nav__item');
 
 
-menuBtn.addEventListener('click', () => {
-    nav.classList.toggle('open');
-    menuNav.classList.toggle('open');
-    navItems.forEach(item => item.classList.toggle('open'))
-});
 
 
-navItems.forEach(item => {
-    item.addEventListener('click', () => {
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-
-        navItems.forEach(item=> item.classList.remove('open'));
-    });
-
-});
-
-
-// Download PDF
-
-const printBtn = document.querySelector("#print-cv");
-
-printBtn.addEventListener("click", () => {
-    window.print();
-})
-
-// Invio Email JS 
+// Form handling 
 
 const contactForm = document.getElementById("contact-form");
 
