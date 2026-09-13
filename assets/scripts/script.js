114796901633a3e927c14d2a1209bb9a7a8667a0
michael-mgc <michael.soto.dev@gmail.com>
2026-09-14 00:38:03 +0200

    feat: complete portfolio

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,19 +3,36 @@
 
 const printBtn = document.querySelector("#print-cv");
 
-
-console.log(printBtn);
-
+if (printBtn) {
 printBtn.addEventListener("click", () => {
     window.print();
 })
-
+}
 
 // Form handling 
+
+emailjs.init({
+    publicKey: "BzQSq9kxDABXrx7Hp"
+});
 
 const contactForm = document.getElementById("contact-form");
 
 contactForm.addEventListener("submit", function(event) {
     event.preventDefault();
+
+    emailjs.sendForm(
+        "service_md4wp1b",
+        "template_io6iz8o",
+        contactForm
+
+    )
+    .then(() => {
+        alert("Messaggio inviato con successo!");
+        contactForm.reset();
+    })
+    .catch((error) => {
+        console.error("Errore:", error);
+        alert("Si è verificato un errore. Riprova.");
+    });
 });
 
